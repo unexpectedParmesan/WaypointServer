44171d5df9e06e5c8897a6b9b84a6fae3667cd66
joshturn <talktojosh@gmail.com>
2015-07-14 16:03:27 -0700

    update config file to connect with cleardb

--- db/config.js
+++ db/config.js
@@ -1,7 +1,7 @@
 var knex = require('knex')({
 	client: 'mysql',
 	connection: {
-		host: '127.0.0.1',
+		host: process.env.DATABASE_URL || '127.0.0.1',
 		user: 'root',
 		password: '',
 		database: 'waypointdb',
