b1963e9274f92c2b01df990fac4482859dc1fce1
joshturn <talktojosh@gmail.com>
2015-07-14 15:24:55 -0700

    add server and db files

---
+++ router.js
@@ -0,0 +1,14 @@
+var router = require('Express').Router();
+var pathController = require('./Controllers/pathController.js');
+// var app = require('./server.js')
+
+module.exports = function(app){
+
+	// NOT IN USE YET. SEE server.js
+
+	// router.route('/')
+	  // .get(pathController.getPath)
+	  // .post(pathController.makePath);
+		// .get(pathController.getAllPaths;
+
+};
