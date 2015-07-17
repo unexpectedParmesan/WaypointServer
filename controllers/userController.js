fd4b1d0b176252029034e5b926d5332a289d26ed
joshturn <talktojosh@gmail.com>
2015-07-17 14:48:32 -0700

    (feat) add methods to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,11 +6,36 @@ var userActiveQuest = require('../db/models/userActiveQuest.js');
  
 module.exports = {
 
+  getUser: function(req, res){
+    new User()
+      .query({where: {facebook_id: req.params.facebook_id}})
+      .fetch()
+      .then(function(user) {
+      if (!user) {
+        res.status(404).send('User not found');
+      } else {
+        res.status(200).send(user);
+      }
+    });
+  },
+
+  makeUser: function(req, res){
+  var newUser = new User({
+  	name: req.body.name,
+  	profile_pic: req.body.profile_pic,
+  	facebook_id: req.body.facebook_id
+  });
+
+  newUser.save().then(function(user){
+  	res.status(200).send(user);
+  })
+  },
+
   getActiveQuests = function(req, res){
-    new Quest().query({where: {user_id: req.params.userId}})
-      .fetchAll()
+    new Quest().query({where: {facebook_id: req.params.facebook_id}})
+      .fetchAll({
       withRelated: 'user_active_quests'
-      .then(function(usersActiveQuests) {
+      }).then(function(usersActiveQuests) {
       res.status(200).send(usersActiveQuests);
     });
   }
