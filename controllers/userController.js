031c9c41b4efcd3318d971318ceb97d294f7b40e
joshturn <talktojosh@gmail.com>
2015-07-17 19:46:56 -0700

    (feat) add methods for userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,7 +26,7 @@ module.exports = {
  },
 
   getActiveQuests = function(req, res){
-    new Quest().query({where: {facebook_id: req.params.facebook_id}})
+    new Quest().query({where: {user_id: req.params.userId}})
       .fetchAll({
       withRelated: 'user_active_quests'
       }).then(function(usersActiveQuests) {
@@ -35,15 +35,40 @@ module.exports = {
   },
 
   updateActiveQuest = function(req, res){
-  
+    new userActiveQuest({
+      user_id: req.params.userId,
+      quest_id: req.params.questId
+    }).fetch().then(function(userActiveQuest){
+      if (!userActiveQuest){
+        res.status(404).send('Quest not found');
+      } else {
+        for (var key in req.body) {
+          userActiveQuest.set(key, req.body[key]);
+        }
+      }
+      userActiveQuest.save().then(function(userActiveQuest){
+        res.status(200).send(userActiveQuest);
+      });
+    });
   },
 
   deleteActiveQuest = function(req, res){
-
+    new userActiveQuest({
+      user_id: req.params.userId,
+      quest_id: req.params.questId
+    }).fetch().then(function(userActiveQuest){
+      if (!userActiveQuest){
+        res.status(404).send('Quest not found');
+      } else {
+        userActiveQuest.destroy().then(function(success){
+          res.status(200).send();
+        });
+        }
+      });
   },
 
   makeActiveQuest = function(req, res){
-
+  
   }
 
 };
