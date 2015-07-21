7db03010aa5b214110555f089e10e978513bd1fd
Colin Parsons <casparsons@gmail.com>
2015-07-21 15:02:51 -0700

    fix user active quest query

--- db/models/userActiveQuest.js
+++ db/models/userActiveQuest.js
@@ -7,10 +7,10 @@ var Waypoint = require('./waypoint.js')
 var UserActiveQuest = db.Model.extend({
 	tableName: 'user_active_quests',
 	user: function() {
-		return this.belongsTo(User);
+		return this.belongsTo('User', 'facebook_id');
 	},
 	quest: function() {
-		return this.belongsTo(Quest).withPivot('current_waypoint_index');
+		return this.belongsTo(Quest);
 	},
 	waypoints: function() {
 		return this.hasMany(Waypoint);
